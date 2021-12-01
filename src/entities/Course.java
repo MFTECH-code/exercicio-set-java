6c036f15f8ad43efc0ae8e3e258d7966fdc9e965
MFTECH-code <matheussilvafeitosa78@gmail.com>
2021-11-30 23:57:37 -0300

    Criação da estrutura base do problema

---
+++ src/entities/Course.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.util.Objects;
+import java.util.Set;
+
+public class Course {
+	private String name;
+	private Set<User> cadastredUsers;
+	
+	public Course() {
+		super();
+	}
+
+	public Course(String name, Set<User> cadastredUsers) {
+		super();
+		this.name = name;
+		this.cadastredUsers = cadastredUsers;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Set<User> getCadastredUsers() {
+		return cadastredUsers;
+	}
+	
+	public void setCadastredUsers(Set<User> cadastredUsers) {
+		this.cadastredUsers = cadastredUsers;
+	}
+	
+	public void signupUser(User user) {
+		cadastredUsers.add(user);
+	}
+	
+	public int totalUsers() {
+		return cadastredUsers.size();
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		return Objects.equals(name, other.name);
+	}
+}
